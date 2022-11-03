9783eb70e8ceab954e09aa4aa7f24451a716f897
Fritjof Lammers <f.lammers@dkfz-heidelberg.de>
2022-11-03 15:48:02 +0100

    no commas

--- workflow/scripts/snpsplit.py
+++ workflow/scripts/snpsplit.py
@@ -8,9 +8,9 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 output_dir = Path(snakemake.output.dir_snp).parent
 
 shell(
-    f"cd {output_dir}; ",
-    "SNPsplit_genome_preparation ",
-    "--vcf_file ../{snakemake.input.vcf_file} ",
-    "--strain {snakemake.params.strain} ",
+    f"cd {output_dir}; "
+    "SNPsplit_genome_preparation "
+    "--vcf_file ../{snakemake.input.vcf_file} "
+    "--strain {snakemake.params.strain} "
     "--reference_genome ../{snakemake.input.ref_genome} {log}"
 )
