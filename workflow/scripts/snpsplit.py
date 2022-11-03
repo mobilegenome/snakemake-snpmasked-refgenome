8d062b2acbe9ed6503478ac2fd22acfdac7f9916
Fritjof Lammers <f.lammers@dkfz-heidelberg.de>
2022-11-03 15:55:44 +0100

    makedir

--- workflow/scripts/snpsplit.py
+++ workflow/scripts/snpsplit.py
@@ -8,6 +8,7 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 output_dir = Path(snakemake.output.dir_snp).parent
 
 shell(
+    f"mkdir -p {output_dir}; "
     f"cd {output_dir}; "
     "SNPsplit_genome_preparation "
     "--vcf_file ../{snakemake.input.vcf_file} "
