f54ce1eb6e61a3ee6e0c9fae771288dcbb8a8ef9
Fritjof Lammers <f.lammers@dkfz-heidelberg.de>
2022-11-04 09:01:08 +0100

    fix paths for run with --directory

--- workflow/scripts/snpsplit.py
+++ workflow/scripts/snpsplit.py
@@ -6,13 +6,13 @@ from snakemake.script import shell
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 output_dir = Path(snakemake.output.dir_snp).parent
-fasta_dir = Path(f"../../../../{snakemake.input.ref_genome}").parent
+fasta_dir = Path(snakemake.input.ref_genome).parent
 
 shell(
     f"mkdir -p {output_dir}; "
     f"cd {output_dir}; "
     "SNPsplit_genome_preparation "
-    "--vcf_file ../../../../{snakemake.input.vcf_file} "
+    "--vcf_file {snakemake.input.vcf_file} "
     "--strain {snakemake.params.strain} "
     f"--reference_genome {fasta_dir} {log}"
 )
