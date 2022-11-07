b2fe17a3b40d7188e2ba8c393ab2bada5de3f026
Fritjof Lammers <f.lammers@dkfz-heidelberg.de>
2022-11-07 13:34:35 +0100

    get first from OutputFiles

--- workflow/scripts/cellranger_mkref.py
+++ workflow/scripts/cellranger_mkref.py
@@ -6,7 +6,7 @@ from snakemake.script import shell
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 input_fasta = Path(snakemake.input.fasta).absolute()
-output_dir = Path(snakemake.output).parent.absolute()
+output_dir = Path(snakemake.output[0]).parent.absolute()
 
 shell(
     f"cd {output_dir} && "
